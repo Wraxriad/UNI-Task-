6f7094eafecea580d0dccff167422e85b88fc45f
Wraxriad <wiihy314@gmail.com>
2026-09-02 09:48:06 +0200

    Revert "Engine"
    This reverts commit 8b6d413e0f8c2f9359686cc2aace8f64d9c59eac.

--- Shaders/CompileShaders.bat
+++
@@ -1,3 +0,0 @@
-C:/VulkanSDK/1.4.335.0/Bin/glslangValidator.exe -V Shader.vert
-C:/VulkanSDK/1.4.335.0/Bin/glslangValidator.exe -V Shader.frag
-pause
--- light.cpp
+++
@@ -1,5 +0,0 @@
-#include "light.h"
-
-Light::Light() {
-    mTransform.position = glm::vec3(-2.0f, 2.0f, 2.0f);
-}
